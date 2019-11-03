63051f4d20540b9e5a8c291d9bfd02e124f9d938
Christian Santos <cesantos647@gmail.com>
2019-11-03 01:45:45 -0800

    update functionality

--- function.js
+++
@@ -1,9 +0,0 @@
-function send(){
-	var variableFromFrontEnd = 2;
-	var socket = io.connect("/");
-	socket.on("connect", function(){
-	    console.log("connected!")
-	    socket.emit("sendVar", variableFromFrontEnd);
-}
-
-io.connect("/").emit("Hello");
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require("http");
+var socketIO = require("socket.io")();
 //setting middleware
 
 app.use(express.static('public')); 
@@ -12,31 +14,9 @@ socketIO.on('connection', function(socket){
 	console.log('a user has connected');
 });
 
-/*socketIO.on('connection', function(socket){
-  console.log('a user connected');
-});
-*/
-
-/* var io = socketIO.listen(server, {log: false});
-
-io.sockets.on("connection", function(socket){
-    socket.on("sendVar", function(value){
-
-        console.log("A user has connected");
-
-    });
-}); */
 
 server.listen(5000);
 
-/*const request = require('request');*/
 
-/*request('', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});*/
-
-
-var server = app.listen(5000);
+//var server = app.listen(5000);
 
