ef59152413cd0be88c159e91445383e8dc415337
Jone Wotsun <ysaito2000@hotmail.com>
2019-11-03 10:49:52 -0800

    Final edition of website

--- index.js
+++ index.js
@@ -5,38 +5,4 @@ var app = express();
 app.use(express.static('public')); 
 app.use('/', express.static('./'));
 
-
-var server = http.createServer(app);
-
-socketIO.on('connection', function(socket){
-	console.log('a user has connected');
-});
-
-/*socketIO.on('connection', function(socket){
-  console.log('a user connected');
-});
-*/
-
-/* var io = socketIO.listen(server, {log: false});
-
-io.sockets.on("connection", function(socket){
-    socket.on("sendVar", function(value){
-
-        console.log("A user has connected");
-
-    });
-}); */
-
-server.listen(5000);
-
-/*const request = require('request');*/
-
-/*request('', { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  console.log(body.url);
-  console.log(body.explanation);
-});*/
-
-
 var server = app.listen(5000);
-
