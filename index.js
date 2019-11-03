2b8b0f8c0f75fa7d082f43cf942bd9163f3b902e
Christian Santos <cesantos647@gmail.com>
2019-11-03 10:01:45 -0800

    update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 var express = require('express');
 var app = express();
 var http = require("http");
-//var socketIO = require("socket.io")();
-//setting middleware
 
+//setting middleware
 app.use(express.static('public')); 
 app.use('/', express.static('./'));
 
-
 var server = http.createServer(app);
-
-/*socketIO.on('connection', function(socket){
-	console.log('a user has connected');
-});*/
-
 
 server.listen(5000);
 
 
-//var server = app.listen(5000);
 
